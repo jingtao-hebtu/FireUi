cb63bcbb2ffe19dc5de06c69ce7a94edb7b5d19f
jingtao-hebtu <jingtao@hebtu.edu.cn>
2025-12-01 11:48:06 +0800

    Add YuvFrameData metatype support

--- Src/FireGUI/FireGuiMain.cpp
+++ Src/FireGUI/FireGuiMain.cpp
@@ -9,12 +9,16 @@
    Brief  :
 **************************************************************************/
 #include <QApplication>
+#include <QMetaType>
 #include "AppMonitor.h"
 #include "FuMainWid.h"
+#include "yuvframedata.h"
 
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    qRegisterMetaType<YuvFrameData>("YuvFrameData");
 
     TF::AppMonitor::initApp(argc, argv);
 
